d906e7f2f05894097e5e568e07076927993ad3e2
Dorjee Dhondup <dhondo01@luther.edu>
2014-05-13 13:30:18 -0500

    calculator class, stack class

---
+++ calculator.cpp
@@ -0,0 +1,9 @@
+#include "calculator.h"
+
+calculator::calculator () {
+	this->make_interface();
+}
+
+calculator::make_interface () {
+	Fl_Window
+}
---
+++ calculator.h
@@ -0,0 +1,18 @@
+// FL
+#include <FL/Fl.H>
+#include <FL/Fl_Window.H>
+#include <FL/Fl_Box.H>
+#include <FL/Fl_Button.H>
+
+#ifndef calculator_h
+#define calculator_h
+
+class calculator {
+private:
+protected:
+public:
+	calculator();
+	make_interface();	
+};
+
+#endif
